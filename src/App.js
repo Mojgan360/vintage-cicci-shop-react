b9ed04ed84ba242b49293c545d2208b62451198c
Mojgan360 <mojgan360@gmail.com>
2020-01-27 22:06:26 +0100

    fixed Router setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import Products from "./pages/Products";
 
 export default function App() {
-  return <h1>hello from app component</h1>;
+  return (
+    <Router>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/cart">
+        <Cart />
+      </Route>
+    </Router>
+  );
 }
