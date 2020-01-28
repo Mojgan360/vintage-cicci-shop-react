bf61d7e6ddada85b7d75e8ec243dc6d7e8c5f231
Mojgan360 <mojgan360@gmail.com>
2020-01-28 20:43:50 +0100

    completed About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return (
+    <section className="about-section section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor amet roof party shaman chambray cliche synth crucifix,
+        drinking vinegar bushwick sriracha lo-fi. Selvage sustainable snackwave,
+        four loko shabby chic etsy photo booth gastropub normcore enamel pin
+        forage church-key drinking vinegar la croix. Umami coloring book
+        post-ironic sartorial knausgaard bicycle rights woke vegan taxidermy
+        before they sold out tousled try-hard. Cold-pressed kinfolk hexagon,
+        succulents iceland offal disrupt 3 wolf moon letterpress pour-over
+        forage tofu green juice pabst irony.
+      </p>
+    </section>
+  );
 }
