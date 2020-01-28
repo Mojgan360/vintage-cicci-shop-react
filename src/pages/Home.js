3c2cbf3dd2fe63c35a3ba60e1f114257dc73037b
Mojgan360 <mojgan360@gmail.com>
2020-01-28 21:15:37 +0100

    completed Home Page & Hero component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import Hero from "../components/Hero";
+import { Link } from "react-router-dom";
 export default function Home() {
   return (
-    <div>
-      <h1>Hello from Home</h1>
-    </div>
+    <>
+      <Hero>
+        <Link to="/products" className="btn btn-primary btn-hero">
+          our products
+        </Link>
+      </Hero>
+    </>
   );
 }
