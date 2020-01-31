fe3ad9117a92194dc1ceddf5a4de3407167bc856
Mojgan360 <mojgan360@gmail.com>
2020-01-31 18:43:08 +0100

    completed ProductDetails page

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,8 +1,44 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+
+import Loading from "../components/Loading";
+import { productContext } from "../context/products";
+// import { cartContext } from "../context/cart";
 
 export default function ProductDetails() {
-  console.log(useParams());
+  const { loading, products } = React.useContext(productContext);
+  // console.log(useParams());
+  const history = useHistory();
   const { id } = useParams();
-  return <h1>hello from product details page, ID is: {id}</h1>;
+
+  const product = products.find(item => item.id === parseInt(id));
+  if (products.length === 0) {
+    return <Loading />;
+  } else {
+    const {
+      image: { url },
+      title,
+      price,
+      description
+    } = product;
+    // const url= image.url
+    return (
+      <section className="single-product">
+        <img src={url} alt="product" className="single-product-image" />
+        <h1>{title}</h1>
+        <h2>{price}</h2>
+        <p>{description}</p>
+        <button
+          className="brn btn-primary btn-block"
+          onClick={() => {
+            console.log("add to cart");
+            history.push("/cart");
+          }}
+        >
+          add to cart
+        </button>
+      </section>
+    );
+  }
 }
