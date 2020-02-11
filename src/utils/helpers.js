10daa8bf70a169ca779644706b39df8b0dacb126
Mojgan360 <mojgan360@gmail.com>
2020-02-11 17:05:29 +0100

    added cloudinary to strapi

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3,8 +3,10 @@ import url from "./URL";
 export function flattenProducts(data) {
   return data.map(item => {
     //cloudinary
-    // let image = item.image.url;
-    let image = `${url}${item.image.url}`;
+    let image = item.image.url;
+
+    //local
+    // let image = `${url}${item.image.url}`;
     return { ...item, image };
   });
 }
