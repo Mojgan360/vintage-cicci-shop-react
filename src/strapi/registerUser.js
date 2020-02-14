1eeb90bde1013f1415f7531c9818efa41f69f1d1
Mojgan360 <mojgan360@gmail.com>
2020-02-14 19:53:17 +0100

    Added login user function

--- src/strapi/registerUser.js
+++ src/strapi/registerUser.js
@@ -1,7 +1,6 @@
 // register user
 import axios from "axios";
 import url from "../utils/URL";
-import React from "react";
 
 const registerUser = async ({ username, email, password }) => {
   const response = await axios
@@ -11,24 +10,3 @@ const registerUser = async ({ username, email, password }) => {
 };
 
 export default registerUser;
-
-// async const registerUser = ({ username, email, password }) => {
-//   const response = axios
-//     .post("http://localhost:1337/auth/local/register", {
-//       username,
-//       email,
-//       password
-//     })
-//     .then(response => {
-//       // Handle success.
-//       console.log("Well done!");
-//       //   console.log("User profile", response.data.user);
-//       //   console.log("User token", response.data.jwt);
-//     })
-//     .catch(error => {
-//       // Handle error.
-//       console.log("An error occurred:", error);
-//     });
-//   return response;
-// };
-// export default registerUser;
