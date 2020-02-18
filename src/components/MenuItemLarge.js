d47fc735c852d5c238937811bc19307b6648a04d
Mojgan360 <mojgan360@gmail.com>
2020-02-18 15:51:26 +0100

    Fixed css login-logout

--- src/components/MenuItemLarge.js
+++ src/components/MenuItemLarge.js
@@ -2,38 +2,36 @@ import React from "react";
 
 import { Link } from "react-router-dom";
 import logo from "../assets/logo1.svg";
+import { UserContext } from "../context/user";
+import LoginLink from "../components/LoginLink";
 
 import CartLink from "../components/Cart/CartLink";
 
 export default function MenuItemLarge() {
+  const { user } = React.useContext(UserContext);
   return (
     <div>
       <div className="large-menu">
         <img src={logo} alt="logo" />
 
-        <nav className=" larg-nav ">
+        <nav>
           <ul>
             <li>
-              <Link to="/" className="link-header">
-                home
-              </Link>
+              <Link to="/">home</Link>
             </li>
             <li>
-              <Link to="/products" className="link-header">
-                Products
-              </Link>
+              <Link to="/products">Products</Link>
             </li>
 
             <li>
-              <Link to="/about" className="link-header">
-                about
-              </Link>
+              <Link to="/about">about</Link>
             </li>
-            <li>
-              <Link to="/login" className="link-header">
-                login
-              </Link>
-            </li>
+            {user.token && (
+              <li>
+                <Link to="/checkout">checkout</Link>
+              </li>
+            )}
+            <LoginLink />
             <CartLink />
           </ul>
         </nav>
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { CartContext } from "../context/cart";
-//import { UserContext } from "../context/user";
+import { UserContext } from "../context/user";
 // import localCart from "../utils/localCart";
 
 import CartItem from "../components/Cart/CartItem";
@@ -8,9 +8,10 @@ import EmptyCart from "../components/Cart/EmptyCart";
 import { Link } from "react-router-dom";
 
 export default function Cart() {
-  let user = false;
+  // let user = false;
 
   const { cart, total } = React.useContext(CartContext);
+  const { user } = React.useContext(UserContext);
   // console.log(cart);
   // console.log(total);
   if (cart.length === 0) {
@@ -26,8 +27,8 @@ export default function Cart() {
       </div>
       <h2>total: ${total}</h2>
 
-      {user ? (
-        <Link to="chekout" className="btn btn-primary">
+      {user.token ? (
+        <Link to="chekout" className="btn btn-primary btn-block">
           checkout
         </Link>
       ) : (
