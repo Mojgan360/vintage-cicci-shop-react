9990a8955daae46a8f83a69e33d7d801f1829e03
Mojgan360 <mojgan360@gmail.com>
2020-02-19 17:30:51 +0100

    Added PropsType product

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,18 +1,28 @@
 import React from "react"; //component/product.js
 import { Link } from "react-router-dom";
+//impt
+import PropTypes from "prop-types";
+import imge from "../../assets/mainBcg.jpeg";
+
 export default function Product({ id, title, price, image }) {
   return (
     <article className="product">
       <div className="img-container">
-        <img src={image} alt="title" />
+        <img src={image || imge} alt={title || "default title"} />
         <Link to={`/products/${id}`} className="btn btn-primary product-link">
           details
         </Link>
       </div>
       <div className="product-footer">
-        <p className="product-title">{title}</p>
-        <p className="product-price">$ {price}</p>
+        <p className="product-title">{title || "default title"}</p>
+        <p className="product-price">$ {price || 0}</p>
       </div>
     </article>
   );
 }
+Product.propTypes = {
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  id: PropTypes.number.isRequired
+};
