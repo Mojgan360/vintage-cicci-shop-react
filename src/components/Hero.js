5bf0927acf0ff1bfce9c788f9b11fc53294a0b4a
Mojgan360 <mojgan360@gmail.com>
2020-02-21 15:48:20 +0100

    find a bug

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,8 +4,8 @@ export default function Hero({ children }) {
   return (
     <div className="hero">
       <div className="banner">
-        <h1>Love Vintage.</h1>
-        <p>work is still</p>
+        <h1>Work is still</h1>
+        <p>........</p>
         <p>going on ...</p>
 
         {children}
